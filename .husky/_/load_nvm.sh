6b2f51d5bd02c125f78df522d77acc2f1dcabf1c
Frank <franklan118@gmail.com>
2022-10-09 09:53:16 +1100

    add load nvm helper

---
+++ .husky/_/load_nvm.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+load_nvm() {
+    if [ -n "$NVM_DIR" ]; then
+        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+        nvm install
+        echo "nvm loaded"
+
+        # Install yarn if not installed
+        if ! command_exists yarn; then
+            echo "yarn not found, installing yarn"
+            npm install -g yarn
+        fi
+    else
+        echo "nvm not found"
+    fi
+}
