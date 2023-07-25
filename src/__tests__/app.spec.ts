455b9e9ff1dc5023d93eb5382e17858080196814
Frank Lan <frank.lan@rea-group.com>
2023-07-25 14:23:03 +1000

    add example for jest spy

--- src/__tests__/app.spec.ts
+++ src/__tests__/app.spec.ts
@@ -1,7 +1,15 @@
-import { hello } from '../app';
+import * as app from '../app';
 
 describe('app', () => {
-  it(`should return 'hello world'`, async () => {
-    expect(await hello()).toBe('Hello World!');
+  it.concurrent(`should return 'hello world'`, async () => {
+    expect(await app.hello()).toBe('Hello World!');
+  });
+
+  describe('GIVEN mocked response', () => {
+    it.concurrent('should return mocked value', async () => {
+      const spy = jest.spyOn(app, 'hello').mockReturnValue(Promise.resolve('mocked value'));
+      expect(await app.hello()).toBe('mocked value');
+      spy.mockRestore();
+    });
   });
 });
