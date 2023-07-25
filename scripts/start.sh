aa2a2ce8e6e53a3a66611ad96e720ef16f6d4b81
Frank Lan <frank.lan@rea-group.com>
2023-07-25 15:00:21 +1000

    carry some changes from git branch `backup/wip/fix-dev-env`

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,12 +2,6 @@
 
 cd "$( dirname "${BASH_SOURCE[0]}" )/.." || exit 1
 
-if [ ! -d "dist" ]; then
-  echo "dist directory not found. Running yarn build..."
-  yarn build
-else
-  echo "dist directory found. Using existing build."
-fi
-
+yarn build
 export NODE_OPTIONS="--experimental-specifier-resolution=node --no-warnings"
 node dist/main.js "$@"
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/main.ts',
   target: 'async-node18.12',
+  mode: 'production',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
