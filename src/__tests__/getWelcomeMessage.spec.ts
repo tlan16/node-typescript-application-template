764d91f0cdfab0dcd4d2af5d5e9b04d6bcad5053
Frank Lan <frank.lan@rea-group.com>
2023-11-15 10:51:52 +1100

    WIP: mocking

--- src/__tests__/getWelcomeMessage.spec.ts
+++ src/__tests__/getWelcomeMessage.spec.ts
@@ -1,10 +1,23 @@
 import assert from "node:assert";
 import {describe, it} from "node:test";
 import {getWelcomeMessage} from "../getWelcomeMessage";
+import * as getWelcomeMessageLib from "../getWelcomeMessage";
 
 void describe('getWelcomeMessage', () => {
   void it("SHOULD return non-empty string", () => {
     const actual = getWelcomeMessage();
     assert.strictEqual(actual.length > 0, true)
+  })
+
+  void describe("GIVEN mocked welcome message", () => {
+    void it("SHOULD return mocked welcome message", (t) => {
+      const expected = "Hello, World!";
+
+      const fn = t.mock.method(getWelcomeMessageLib, "getWelcomeMessage");
+      fn.mock.mockImplementation(() => expected);
+
+      const actual = getWelcomeMessage();
+      assert.strictEqual(actual, expected)
+    })
   })
 });
