d18578d2d09ce7fcd9d6a10444693e1264363fe9
Frank Lan <frank.lan@rea-group.com>
2023-11-15 12:11:46 +1100

    clean up

--- src/getWelcomeMessage.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getWelcomeMessage = void 0;
-var getWelcomeMessage = function () {
-    return 'Welcome to the app';
-};
-exports.getWelcomeMessage = getWelcomeMessage;
--- src/main.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var getWelcomeMessage_js_1 = require("./getWelcomeMessage.js");
-console.log((0, getWelcomeMessage_js_1.getWelcomeMessage)());
